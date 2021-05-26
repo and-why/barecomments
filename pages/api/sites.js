e9bb823d2f944209880ba4c38488fbc4c874d80b
Andy Smith <work@andysmith.is>
2021-05-27 07:10:28 +1000

    bugs

--- pages/api/sites.js
+++ pages/api/sites.js
@@ -5,8 +5,8 @@ const siteData = async (req, res) => {
   try {
     const { uid } = await auth.verifyIdToken(req.headers.token);
 
-    const sites = await getUserSites(uid);
-    res.status(200).json(sites);
+    const { sites } = await getUserSites(uid);
+    res.status(200).json({ sites });
   } catch (error) {
     res.status(500).json({ error });
   }
